fd426e3a91d50e9b573071ed2547ceee2ebfc3e8
Your Name <you@example.com>
2020-09-13 21:53:05 +0800

    second commit

--- app/src/main/java/com/example/jizhangapp/MainActivity.java
+++ app/src/main/java/com/example/jizhangapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.jizhangapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//test first upload
 public class MainActivity extends AppCompatActivity {
 
     @Override
