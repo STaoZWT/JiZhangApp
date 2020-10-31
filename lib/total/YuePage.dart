4bdde0829642bfb5319c113480f93128af78b198
STaoZWT <zhangwt2014@gmail.com>
2020-11-01 00:40:12 +0800

    修复bugs

--- lib/total/YuePage.dart
+++ lib/total/YuePage.dart
@@ -704,7 +704,7 @@ class _YuePageContentState extends State<YuePageContent>
                             return new Container(
                                 height: 72,
                                 child: Card(
-                                    margin: EdgeInsets.all(5.0),
+                                    margin: EdgeInsets.all(1.0),
                                     elevation: 2.0,
                                     color: Colors.white,
                                     shape: const RoundedRectangleBorder(
